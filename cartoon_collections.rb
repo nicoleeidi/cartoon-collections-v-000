6f1a16e7c7438861192071f11bb768f8a573eefa
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-30 17:20:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |name|
-    capname=name[0].upcase
+    capname=name.titelize
     capname << "!"
     binding.pry
   end
